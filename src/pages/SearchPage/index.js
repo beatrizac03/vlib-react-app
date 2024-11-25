44ff693bc0be99f4bb165a8fb7d7544e663fcc61
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-11-25 12:28:39 -0300

    fix: searching for categories or authors

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import { useLocation, useParams } from "react-router-dom";
 import { useLivrosContext } from "../../context/ContextLivro";
 import CardLivro from "../../components/CardLivro";
+import styles from "./SearchPage.module.css";
 
 export default function SearchPage() {
   const { livros } = useLivrosContext(); // retorna a lista de livros do Context
@@ -12,26 +13,37 @@ export default function SearchPage() {
 
   const livrosCorrespondentes = livros.filter((livro) => {
     const title = livro.title?.toLowerCase() || "";
-    const category = livro.category?.toLowerCase() || "";
+
+    const categories = livro.categories?.some((category) =>
+      category.toLowerCase().includes(searchQuery)
+    );
+
+    const authors = livro.authors?.some((author) =>
+      author.toLowerCase().includes(searchQuery)
+    );
+
     return (
-      searchQuery &&
-      (title.includes(searchQuery) || category.includes(searchQuery))
+      searchQuery && (title.includes(searchQuery) || categories || authors)
     );
   });
 
   return (
     <>
       <Header />
-      <div>
-        <h2>Resultados para: {searchQuery} </h2>
+      <div className={styles.containAll}>
+        <div className={styles.result}>
+          <h2>Resultados para: {searchQuery} </h2>
+        </div>
+        <div className={styles.containerResults}>
+          {livrosCorrespondentes.length > 0 ? (
+            livrosCorrespondentes.map((livro) => (
+              <CardLivro key={livro.id} livro={livro} />
+            ))
+          ) : (
+            <p>Nenhum resultado encontrado para "{searchQuery}"</p>
+          )}
+        </div>
       </div>
-      {livrosCorrespondentes.length > 0 ? (
-        livrosCorrespondentes.map((livro) => (
-          <CardLivro key={livro.id} livro={livro} />
-        ))
-      ) : (
-        <p>Nenhum resultado encontrado para "{searchQuery}"</p>
-      )}
     </>
   );
 }
